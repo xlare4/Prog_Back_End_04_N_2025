6bc5f49ca5e2a910bc1aecd1ad59b70b64831e0b
profHebert <hebert@uni9.pro.br>
2025-08-14 15:44:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,8 +8,7 @@
 <body>
     <h1>Programação Back-End - PHP</h1>
     <?php
-    $a="<hr>";
+    echo "Comando pelo php $a novo";
     ?>
-    <?="Comando pelo php $a novo";?>
 </body>
 </html>
