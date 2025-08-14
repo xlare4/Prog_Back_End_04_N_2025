6ae1ad3ff24be31da20c2940000053c156a7747f
profHebert <hebert@uni9.pro.br>
2025-08-14 15:42:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Programação Back-End - PHP</title>
 </head>
 <body>
-    <h1>Testando php</h1>
+    <h1>Programação Back-End - PHP</h1>
     <?php
     $a="<hr>";
     ?>
